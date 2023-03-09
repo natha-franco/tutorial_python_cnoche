f2b9ae8875c04ea067d31f385d5e685e0d9fd3f0
natha-franco <naticafranco34@gmail.com>
2023-03-08 19:53:27 -0500

    Se agrega los tipos de datos

---
+++ repasoPython/main.py
@@ -0,0 +1,25 @@
+#Tipos de datos
+#Numeros enteros
+
+numero1: int = 78 #tipado estatico
+numero2 = 45 #tipado dinamico
+print(numero1)
+print(numero2)
+
+#Numeros reales
+
+numero3: float = 4.9
+numero4 = 5.0
+print(numero3)
+print(numero4)
+
+#Booleanos toma dos valores Verdadero o Falso
+
+esColombiano: bool = True
+esArgentino = False
+print(esColombiano)
+print(esArgentino)
+
+#Caracter y Cadena de catacteres
+mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
+print(mensaje)
